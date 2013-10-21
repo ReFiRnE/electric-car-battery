810cf0af96693724631a4ef869d476f227074414
nikol4u1337@gmail.com <nikol4u1337@gmail.com@beccfabe-aef7-59e9-ed7f-41e2d3f7ddf7>
2013-10-21 18:06:24 +0000

    Basic of graph, traversing to do

---
+++ trunk/src/DomainLayer/Edge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer
+{
+    /// <summary>
+    /// Represents an edge in a our map
+    /// </summary>
+    public class Edge
+    {
+        private BatteryStation _from;
+        private BatteryStation _to;
+
+        public Edge(BatteryStation From, BatteryStation To)
+        {
+            this._from = From;
+            this._to = To;
+        }
+
+        public BatteryStation From
+        {
+            get
+            {
+                return _from;
+            }
+        }
+
+        public BatteryStation To
+        {
+            get
+            {
+                return _to;
+            }
+        }
+    }
+}
--- trunk/src/DomainLayer/Graph.cs
+++ trunk/src/DomainLayer/Graph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,138 @@ namespace DomainLayer
 {
     class Graph
     {
+        private IList<BatteryStation> stations;
+
+        public Graph()
+        {
+            stations = new List<BatteryStation>();
+        }
+
+        public bool IsEmpty()
+        {
+            return stations.Count == 0;
+        }
+
+        public void AddStation(BatteryStation station)
+        {
+            if (!stations.Contains(station))
+                stations.Add(station);
+        }
+
+        public void AddEdge(BatteryStation from, BatteryStation to)
+        {
+            if (FindEdge(from, to) == null)
+                from.AddEdge(to);
+        }
+
+        public bool ContainsVertex(BatteryStation station)
+        {
+            return stations.Contains(station);
+        }
+
+        public bool IsAdjacent(BatteryStation startStation, BatteryStation endStation)
+        {
+            return (FindEdge(startStation, endStation) != null);
+        }
+
+        public IList<BatteryStation> GetAdjacencies(BatteryStation station)
+        {
+            List<BatteryStation> adjStations = new List<BatteryStation>();
+            IEnumerator ien = station.EdgeList().GetEnumerator();
+
+            while (ien.MoveNext())
+            {
+                Edge e = (Edge)ien.Current;
+                adjStations.Add(e.To);
+            }
+
+            return adjStations;
+        }
+
+        public IEnumerator<BatteryStation> BatteryStations()
+        {
+            return stations.GetEnumerator();
+        }
+
+        public int GetNoOfVertices()
+        {
+            return stations.Count();
+        }
+
+        public int GetNoOfEdges()
+        {
+            int noOfEdges = 0;
+
+            foreach (BatteryStation station in stations)
+                noOfEdges += station.OutDegree();
+
+            return noOfEdges;
+        }
+
+        public void Clear()
+        {
+            stations.Clear();
+        }
+
+        public void RemoveEdge(BatteryStation from, BatteryStation to)
+        {
+            Edge e = FindEdge(from, to);
+
+            if (e != null)
+                e.From.RemoveEdge(e);
+        }
+
+
+        public void RemoveBatteryStation(BatteryStation station)
+        {
+            stations.Remove(station);
+
+            IEnumerator ien = stations.GetEnumerator();
+
+            while (ien.MoveNext())
+            {
+                BatteryStation bs = (BatteryStation)ien.Current;
+                IList<Edge> stationEdges = bs.EdgeList();
+
+                for (int i = 0; i < stationEdges.Count; i++)
+                {
+                    Edge e = stationEdges[i];
+
+                    if (e.To.Equals(station))
+                        stationEdges.Remove(e);
+                }
+            }
+        }
+
+        public Edge FindEdge(BatteryStation from, BatteryStation to)
+        {
+            Edge edge = null;
+            IEnumerator ienum = from.EdgeList().GetEnumerator();
+
+            while ((edge != null) && ienum.MoveNext())
+            {
+                if (ienum.Current.Equals(to))
+                    edge = (Edge)ienum.Current;
+            }
+
+            return edge;
+        }
+
+        #region Traversing
+        public void Bfs(BatteryStation v)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dfs(BatteryStation v)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Unmark()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
     }
 }
