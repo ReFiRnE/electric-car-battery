fb83d7ff63a95726400cdf642b6dad7c6d5ec1ea
nikol4u1337 <nikol4u1337@gmail.com>
2013-10-21 18:06:24 +0000

    Basic of graph, traversing to do

--- src/DomainLayer/BatteryStation.cs
+++ src/DomainLayer/BatteryStation.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace DomainLayer
 {
-    class BatteryStation
+    public class BatteryStation
     {
+        // what we need for the battery station itself
+        private int stationID { get; set; }
+        private string name { get; set; }
+        private DateTime timeOpen { get; set; }
+        private DateTime timeClose { get; set; }
+
+        // what we need for the graph + the name
+        private bool mark { get; set; }
+        private IList<Edge> edges;
+
+        public BatteryStation(int stationID, string name, DateTime timeOpen, DateTime timeClose)
+        {
+            this.stationID = stationID;
+            this.name = name;
+            this.timeOpen = timeOpen;
+            this.timeClose = timeClose;
+
+            mark = false;
+            edges = new List<Edge>();
+        }
+
+        public void AddEdge(BatteryStation station)
+        {
+            Edge newEdge = new Edge(this, station);
+            edges.Add(newEdge);
+        }
+
+        public int OutDegree()
+        {
+            return edges.Count;
+        }
+
+        public void RemoveEdge(Edge edge)
+        {
+            edges.Remove(edge);
+        }
+
+        public IList<Edge> EdgeList()
+        { 
+            return edges; 
+        }
     }
 }
